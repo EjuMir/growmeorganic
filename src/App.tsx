a4938fc8827515b5af2ee82da21021981d0152ea
EjuMir <mirejo101@gmail.com>
2024-09-15 00:28:13 +0600

    data is fetching successfully but api pagination need work

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,28 @@ import { Column } from 'primereact/column';
 
 export default function PaginatorBasicDemo() {
     const [customers, setCustomers] = useState([]);
+    const [first, setFirst] = useState<number>(0); 
+    const [rows, setRows] = useState<number>(10);   
+    const [currentPage, setCurrentPage] = useState<number>(0);
 
-    useEffect(() => {
-         fetch('https://api.artic.edu/api/v1/artworks?page=1')
+    const sendPageNumber = (page:number) => {
+      fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`)
          .then(response => response.json())
-         .then(data => console.log(data))
-    }, []);
+         .then(data => setCustomers(data.data))
+  };
+    useEffect(() => {
+         sendPageNumber(currentPage)
+    }, [currentPage]);
+
+    const search= (event) =>{
+      setFirst(event.first);
+      setRows(event.rows); 
+      setCurrentPage(event.page);
+    }
 
     return (
         <div className="card">
-            <DataTable value={customers} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
+            <DataTable value={customers} onPage={search} paginator rows={rows} first={first} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
                 <Column field="name" header="Name" style={{ width: '25%' }}></Column>
                 <Column field="country.name" header="Country" style={{ width: '25%' }}></Column>
                 <Column field="company" header="Company" style={{ width: '25%' }}></Column>
@@ -24,3 +36,4 @@ export default function PaginatorBasicDemo() {
     );
 }
         
+
